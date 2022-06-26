b79e51debd2c10fa3044f709feae1fda7c777722
AvivBenY <benyedidia123@gmail.com>
2022-06-26 22:18:00 +0300

    mongoDB & mongoose installation, new CRUD methods

--- server.js
+++ server.js
@@ -1,115 +1,90 @@
 import fsp from 'fs/promises'
 import express from 'express'
+import mongoose from 'mongoose'
 
 const app = express();
-
 app.use(express.json());
 
-function maxId(arr) {
-    const ids = arr.map(object => {
-        return object.id;
-    });
-    const max = Math.max(...ids);
-    return max
+
+const db = mongoose.model(
+    "Product", {
+    title: String,
+    price: Number,
+    description: String,
+    category: String,
+    image: String,
 }
+)
 
-//QUERY
+//GET - QUERY
 app.get('/products', (req, res) => {
-    fsp.readFile('./products.json', 'utf8').then((data) => {
-        const jsonData = JSON.parse(data);
-        console.log("query", req.query);
-        if (req.query) {
-            const { title } = req.query;
-            const filterProducts = jsonData.filter((p) =>
+    const { title } = req.query;
+    if (title) {
+        db.find().then((data) => {
+            const filterProducts = data.filter((p) =>
                 p.title.toLowerCase().includes(title.toLowerCase()))
-            console.log(filterProducts);
             res.send(filterProducts);
-        }
-        res.send(jsonData)
-    })
+        }).catch((e) => console.log("error", e))
+    } else { res.send(db.find()) }
 })
 
-
-// READ
-app.get("/products", (req, res) => {
-    fsp.readFile('./products.json', 'utf8').then(data => {
-        const jsonData = JSON.parse(data);
-        const stringData = JSON.stringify(jsonData);
-        res.send(stringData);
-    })
+//GET ALL PRODUCTS
+app.get('/products', (req, res) => {
+    db.find().then((data) => res.send(data)).catch((e) => console.log("error", e));
 })
 
-//READ BY ID
-app.get('/products/:productId', (req, res) => {
-    const { productId } = req.params;
-    fsp.readFile('./products.json', 'utf8').then(data => {
-        const jsonData = JSON.parse(data);
-        const product = jsonData.find((p) => p.id === +productId)
-        if (product) {
+// //GET PRODUCT BY ID
+app.get('/products/:id', (req, res) => {
+    const { id } = req.params;
+    if (id) {
+        db.findById(id).then((product) => {
             res.send(product);
-        } else {
-            res.send("cant get product");
-        }
-    })
+        }).catch((e) => console.log("error", e));
+    }
+})
+
+// //CREATE
+app.post('/products', (req, res) => {
+    const { title } = req.body;
+    const { price } = req.body;
+    const { description } = req.body;
+    const { category } = req.body;
+    const { image } = req.body;
+    db.insertMany([{
+        title,
+        price,
+        description,
+        category,
+        image
+    }]).then((data) => res.send(data))
 })
 
 
-//CREATE
-app.post('/products', (req, res) => {
-    console.log("req.body", req.body);
-    fsp.readFile('./products.json', 'utf8').then((data) => {
-        const products = JSON.parse(data);
-        const { title } = req.body.title;
-
-        products.push({
-            id: maxId(products) + 1,
-            title: req.body.title,
-            price: req.body.price,
-            description: req.body.description,
-            category: req.body.category,
-            image: req.body.image,
-            rating: {
-                rate: 3.9,
-                count: 120
-
-            }
-        });
-        fsp.writeFile('./products.json', JSON.stringify(products));
-        console.log(req.body);
-        res.send(products)
+// //DELETE BY ID
+app.delete('/products/:id', (req, res) => {
+    const { id } = req.params;
+    if (id) {
+        db.findByIdAndRemove(id).then((data) => {
+            res.send(data);
+        }).catch((e) => console.log("error", e))
     }
-    )
 })
 
 //UPDATE
-app.patch('/products/:productId', (req, res) => {
-    const { productId } = req.params;
-    const { title } = req.body;
-    fsp.readFile('./products.json', 'utf8').then((data) => {
-        if (req.body) {
-            const jsonData = JSON.parse(data);
-            const index = jsonData.findIndex((p) => p.id === +productId);
-            jsonData[index] = { ...jsonData[index], ...req.body };
-            console.log("data[index]", jsonData[index]);
-            fsp.writeFile('./products.json', JSON.stringify(jsonData)).then(() => res.send(jsonData))
-        }
-    }).catch((e) => console.log("error"))
-})
-
-//DELETE
-app.delete('/products/:id', (req, res) => {
-    const { id } = req.params;
-    fsp.readFile('./products.json', 'utf8').then((data) => {
-        const jsonData = JSON.parse(data);
-        const index = jsonData.findIndex((p) => p.id === +id);
-        if (index >= 0) {
-            jsonData.splice(index, 1);
-            fsp.writeFile('./products.json', JSON.stringify(jsonData)).then(() => res.send(jsonData));
-            res.send(jsonData);
-        }
-    }).catch((e) => console.log("ERRORRR", e))
+app.patch('/products/:id', (req, res) => {
+    const id = req.params;
+    console.log("id", id);
+    console.log("body", req.body);
+    if (id) {
+        db.findByIdAndUpdate(id, req.body).then((p) => {
+            console.log("P:", p);
+            res.send(p);
+        }).catch((e) => console.log("error", e))
+    }
 })
 
 
-
-app.listen('8000');
+mongoose.connect('mongodb://localhost:27017/gocode-shop').then(() => {
+    console.log("db connected");
+    app.listen(8000);
+})
